0856f9f164645e6857e8a4f72c5d7eeb313a4946
Werbeth <werbeth1991pbs@gmail.com>
2022-08-15 12:57:27 -0300

    feat: add file routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const http = require("http");
-const UserController = require("./controllers/UserController");
+const routes = require("./routes");
 
 const port = 3000;
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  if (request.url === "/users" && request.method === "GET") {
-    UserController.listUsers(request, response);
+  const route = routes.find(
+    (objRoutes) =>
+      objRoutes.endpoint === request.url && objRoutes.method === request.method
+  );
+
+  if (route) {
+    route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-type": "text/html" });
     response.end(`Cannot ${request.method} ${request.url}`);
