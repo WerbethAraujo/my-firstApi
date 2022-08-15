b58da966674b1af3c8a4914ec445fedd7c8fe0d4
Werbeth <werbeth1991pbs@gmail.com>
2022-08-15 14:21:38 -0300

    add sorting with query params

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,18 @@
 const users = require("../mocks/users");
 
 module.exports = {
-  listUsers(resquest, response) {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(JSON.stringify(users));
+  listUsers(request, response) {
+    const { order } = request.query;
+    const sortdUsers = users.sort((antes, dpois) => {
+      if (order === "asc") {
+        return antes.id > dpois.id ? 1 : -1;
+      } else {
+        return antes.id < dpois.id ? 1 : -1;
+      }
+    });
+
+    if (order === "desc")
+      response.writeHead(200, { "Content-Type": "text/html" });
+    response.end(JSON.stringify(sortdUsers));
   },
 };
